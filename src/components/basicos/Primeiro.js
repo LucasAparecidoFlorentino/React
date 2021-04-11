ffecc43a01c66fd9ff64ec0c570d88c3c307fa7f
LucasAparecidoFlorentino <lucasflorentino_2000@hotmail.com>
2021-04-11 19:01:55 -0300

    Criado primeiro componente

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Primeiro() {
+    const msg = 'Seja bem vindo(a) !'
+    // Utilizar parentêses para deixar o código identado em várias linhas
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{ msg }</p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-const tag = <strong>Olá React !!</strong>
+import Primeiro from './components/basicos/Primeiro'
 
+// Realiza a renderização na tela
 ReactDOM.render(
     <div>
-        { tag }
+        <Primeiro></Primeiro>
     </div>,
+    // Apresenta as informações contidas aqui no index
     document.getElementById('root')
 )
