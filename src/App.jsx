1568717d1eed2d405d582e6bb62d1ef20368d130
LucasAparecidoFlorentino <lucasflorentino_2000@hotmail.com>
2021-04-15 17:49:22 -0300

    Aprendendo a utilização do card.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from './components/layout/card'
 
 // Com a utilização do defaul, consigo retornar uma função anonima, caso contrário, erro.
 // =>: para uma arrow function
@@ -11,9 +12,21 @@ import Aleatorio from './components/basicos/Aleatorio'
 export default _ =>
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={5} max={15}/>
-        <Fragmento />
-        <ComParametro Titulo="Situação do Aluno"
-            aluno="Lucas" nota={10} />
-        <Primeiro></Primeiro>
+
+        <Card titulo="#04 - Desafio Aleatório">
+            <Aleatorio min={5} max={15} />
+        </Card>
+
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parametro">
+            <ComParametro Titulo="Situação do Aluno"
+                aluno="Lucas" nota={10} />
+        </Card>
+
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
     </div>
