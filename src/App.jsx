5bcc1f0a94b67554a1b7a35b3b3f04483193bcbf
LucasAparecidoFlorentino <lucasflorentino_2000@hotmail.com>
2021-04-18 21:26:45 -0300

    Desenvolvido Componente Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
+import './App.css'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
-import Card from './components/layout/card'
+import Card from './components/layout/Card'
 
 // Com a utilização do defaul, consigo retornar uma função anonima, caso contrário, erro.
 // =>: para uma arrow function
 // se utilizar apenas um parametro, não precisa de parenteses.
 export default _ =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
 
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={5} max={15} />
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório" color='#FA6900'>
+                <Aleatorio min={5} max={15} />
+            </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
+            <Card titulo="#03 - Fragmento" color='#E94C6F'>
+                <Fragmento />
+            </Card>
 
-        <Card titulo="#02 - Com Parametro">
-            <ComParametro Titulo="Situação do Aluno"
-                aluno="Lucas" nota={10} />
-        </Card>
+            <Card titulo="#02 - Com Parametro" color='#E8B71A'>
+                <ComParametro Titulo="Situação do Aluno"
+                    aluno="Lucas" nota={10} />
+            </Card>
 
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro></Primeiro>
-        </Card>
+            <Card titulo="#01 - Primeiro Componente" color='#588C73'>
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
+
+
     </div>
